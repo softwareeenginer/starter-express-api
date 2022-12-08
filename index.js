7767c469948d41e51ddd851b81a2ae1ffe904f1d
Mehmet Ali Yılgın <57998586+softwareeenginer@users.noreply.github.com>
2022-12-08 20:26:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    console.log(req, res)
+    res.send('IN APP PURCHASE IS LISTENING')
 })
 app.listen(process.env.PORT || 3000)
