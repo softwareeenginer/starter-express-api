ebd9f58b5b781c5e11c8efbc9f2fad6aabef4b5e
Mehmet Ali Yılgın <57998586+softwareeenginer@users.noreply.github.com>
2022-12-21 01:15:18 +0300

    ....

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express')
 const app = express()
+var jose = require('node-jose');
+const aes256 = require('aes256');
+const jwt = require("jsonwebtoken");
+
 app.all('/', (req, res) => {
-    console.log(res.header)
+
+    console.log("Req data: " + req.data);
+
+    console.log("req headers: " + req.headers)
+
+    console.log("req payload:" + req.payload)
+
+
+    /*
+    const decode = jwt.decode(signedPayload);
+    const decodeSignedTransactionInfo = jwt.decode(decode.data.signedTransactionInfo)
+    const decodesignedRenewalInfo = jwt.decode(decode.data.signedRenewalInfo)
+    console.log(decode)
+    console.log("\n\n")
+    console.log(decodeSignedTransactionInfo, decodesignedRenewalInfo)
+    */
     res.send('IN APP PURCHASE IS LISTENING')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
+
