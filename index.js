66fc0a230355c6f6a83856c3c49e6e1c31077dc1
Mehmet Ali Yılgın <57998586+softwareeenginer@users.noreply.github.com>
2023-01-18 23:39:39 +0300

    ...

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const app = express();
-
+const subsRegister = require("./subs-register");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use("/subs-register", subsRegister);
+
 
 app.all('/', (req, res) => {
-    
+
     // -- SIGNED PAYLOAD -- //
-    const signedPayload = req.body.signedPayload;
+    const signedPayload = req.body?.signedPayload;
 
     // -- DECODE -- //
     const decode = jwt.decode(signedPayload);
-    const decodeSignedTransactionInfo = jwt.decode(decode.data.signedTransactionInfo);
-    const decodesignedRenewalInfo = jwt.decode(decode.data.signedRenewalInfo);
+    const decodeSignedTransactionInfo = jwt.decode(decode?.data?.signedTransactionInfo);
+    const decodesignedRenewalInfo = jwt.decode(decode?.data?.signedRenewalInfo);
     console.log(decode);
     console.log("\n\n\n");
     console.log(decodeSignedTransactionInfo, decodesignedRenewalInfo);
---
+++ subs-register.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+
+// -------- LOGIN -------- //
+router.post("/register", async (req, res) => {
+    
+    console.log("veri")
+});
+
+
+module.exports = router;
